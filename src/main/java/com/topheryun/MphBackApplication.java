58a8e0f0f2858f4d4b3c68a98d6a631df39c06ee
topheryun <topheryun@gmail.com>
2021-02-08 09:17:11 -0800

    first commit

---
+++ src/main/java/com/topheryun/MphBackApplication.java
@@ -0,0 +1,13 @@
+package com.topheryun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MphBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MphBackApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/topheryun/models/Mouse.java
@@ -0,0 +1,24 @@
+package com.topheryun.models;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity @Data @NoArgsConstructor @AllArgsConstructor
+public class Mouse {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private int price;
+	private LocalDate date;
+
+}
